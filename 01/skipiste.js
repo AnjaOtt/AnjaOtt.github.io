ce75a491dec5e8957ccb4624e793c4ab5534b0cc
Anja <Anja@Anjas-PC>
2017-10-15 14:00:54 +0200

    upload repository

--- 01/skipiste.js
+++ 01/skipiste.js
@@ -5,19 +5,13 @@ Matrikel: 256342
 Datum: 13.10.17
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.
 */
-
 window.addEventListener("load", init);
-
-function init(): void {
-    let canvas: HTMLCanvasElement = document.getElementsByTagName("canvas")[0];
+function init() {
+    let canvas = document.getElementsByTagName("canvas")[0];
     console.log(canvas);
-
-    let crc2: CanvasRenderingContext2D = canvas.getContext("2d");
+    let crc2 = canvas.getContext("2d");
     console.log(crc2);
-    
-   
-  /*Himmel*/
-    
+    /*Himmel*/
     crc2.beginPath();
     crc2.moveTo(600, 0);
     crc2.lineTo(0, 400);
@@ -26,16 +20,12 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#DDEBF7";
     crc2.fill();
-    
     /*Sonne*/
-    
     crc2.beginPath();
     crc2.arc(60, 50, 40, 0, 2 * Math.PI);
     crc2.fillStyle = "#EED531";
     crc2.fill();
-    
     /*Wolke*/
-    
     crc2.beginPath();
     crc2.arc(103, 70, 20, 0, 2 * Math.PI);
     crc2.fillStyle = "#ffffff";
@@ -51,7 +41,6 @@ function init(): void {
     crc2.arc(130, 70, 20, 0, 2 * Math.PI);
     crc2.fillStyle = "#ffffff";
     crc2.fill();
-    
     /*Berge*/
     crc2.beginPath();
     crc2.moveTo(0, 260);
@@ -62,7 +51,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#91A292";
     crc2.fill();
-    
     crc2.beginPath();
     crc2.moveTo(0, 320);
     crc2.lineTo(320, 98);
@@ -72,7 +60,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#91A292";
     crc2.fill();
-    
     /*Lift*/
     crc2.beginPath();
     crc2.moveTo(750, 100);
@@ -84,7 +71,6 @@ function init(): void {
     crc2.lineTo(775, 100);
     crc2.closePath();
     crc2.stroke();
-    
     crc2.beginPath();
     crc2.moveTo(550, 300);
     crc2.lineTo(550, 370);
@@ -95,7 +81,6 @@ function init(): void {
     crc2.lineTo(575, 300);
     crc2.closePath();
     crc2.stroke();
-    
     crc2.beginPath();
     crc2.moveTo(350, 500);
     crc2.lineTo(350, 570);
@@ -106,33 +91,27 @@ function init(): void {
     crc2.lineTo(375, 500);
     crc2.closePath();
     crc2.stroke();
-    
     crc2.beginPath();
     crc2.moveTo(800, 50);
     crc2.lineTo(750, 100);
     crc2.closePath();
     crc2.stroke();
-   
     crc2.beginPath();
     crc2.moveTo(750, 100);
     crc2.lineTo(550, 300);
     crc2.closePath();
     crc2.stroke();
-    
     crc2.beginPath();
     crc2.moveTo(550, 300);
     crc2.lineTo(350, 500);
     crc2.closePath();
     crc2.stroke();
-    
     crc2.beginPath();
     crc2.moveTo(350, 500);
     crc2.lineTo(250, 600);
     crc2.closePath();
     crc2.stroke();
-    
     /*Baum1*/
-    
     crc2.beginPath();
     crc2.moveTo(760, 260);
     crc2.lineTo(730, 290);
@@ -141,7 +120,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#315A10";
     crc2.fill();
-    
     crc2.beginPath();
     crc2.moveTo(760, 240);
     crc2.lineTo(730, 270);
@@ -150,7 +128,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#315A10";
     crc2.fill();
-    
     crc2.beginPath();
     crc2.moveTo(760, 220);
     crc2.lineTo(730, 250);
@@ -159,7 +136,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#315A10";
     crc2.fill();
-    
     /*Baum2*/
     crc2.beginPath();
     crc2.moveTo(670, 340);
@@ -169,7 +145,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#549222";
     crc2.fill();
-    
     crc2.beginPath();
     crc2.moveTo(670, 320);
     crc2.lineTo(640, 350);
@@ -178,7 +153,6 @@ function init(): void {
     crc2.stroke();
     crc2.fillStyle = "#549222";
     crc2.fill();
-    
     crc2.beginPath();
     crc2.moveTo(670, 300);
     crc2.lineTo(640, 330);
@@ -186,5 +160,6 @@ function init(): void {
     crc2.closePath();
     crc2.stroke();
     crc2.fillStyle = "#549222";
-    crc2.fill(); 
+    crc2.fill();
 }
+//# sourceMappingURL=skipiste.js.map
