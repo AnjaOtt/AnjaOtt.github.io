7d72e65a1a46848fdc8454497bed57285153c800
Anja <Anja@LAPTOP-6AOH02PO.fritz.box>
2017-12-20 12:16:20 +0100

    corrected A09

--- 09/erpresserbrief.js
+++ 09/erpresserbrief.js
@@ -8,10 +8,12 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 var A09;
 (function (A09) {
     window.addEventListener("load", init);
-    // window.addEventListener("keydown", handleKeydown);
+    window.addEventListener("keydown", handleKeyDown);
     let text;
+    //String-Array zum Generieren der Buchstaben
     let letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
     let saveLetter = "";
+    // Div-Boxen f�r Alphabeth erstellen und gestalten
     function init() {
         for (let i = 0; i < letters.length; i++) {
             let l = document.createElement("div");
@@ -27,7 +29,8 @@ var A09;
             l.addEventListener("click", handleLetterClick);
             document.body.appendChild(l);
         }
-        let box = document.createElement("div"); //gro�e Textbox erstellen und gestalten
+        //Brieffeld erstellen und gestalten
+        let box = document.createElement("div");
         box.style.width = "1165px";
         box.style.height = "500px";
         box.style.marginTop = "20px";
@@ -35,6 +38,7 @@ var A09;
         box.addEventListener("click", handleBoxClick);
         document.body.appendChild(box);
     }
+    // Funktion zum Hervorheben des geklickten Buchstaben
     function handleLetterClick(_event) {
         console.log(_event.target);
         let l = _event.target;
@@ -48,6 +52,16 @@ var A09;
             }
         }
     }
+    //Funktion f�r Tastatureingabe
+    function handleKeyDown(_event) {
+        if (_event.keyCode > 64 && _event.keyCode < 91 || _event.keyCode == 16 || _event.keyCode == 20) {
+            saveLetter = String.fromCharCode(_event.keyCode);
+        }
+        else {
+            return;
+        }
+    }
+    // Funktion zum Plazieren des gespeicherten Buchstabens in dem Brieffeld
     function handleBoxClick(_event) {
         if (saveLetter == "")
             return;
@@ -61,8 +75,17 @@ var A09;
         text.style.backgroundColor = "hsl(" + Math.random() * 360 + ", 100%, 80%)";
         text.style.left = _event.pageX + "px";
         text.style.top = _event.pageY + "px";
-        //text.addEventListener("click", deleteLetter);
+        text.addEventListener("click", deleteLetter);
         document.body.appendChild(text);
+    }
+    //Funktion zum L�schen eines Buchstabens durch gedr�ckte Alt-Taste und Klicken
+    function deleteLetter(_event) {
+        if (_event.altKey == false)
+            return;
+        else {
+            let remove = _event.target;
+            document.body.removeChild(remove);
+        }
     }
 })(A09 || (A09 = {}));
 //# sourceMappingURL=erpresserbrief.js.map
--- 09/erpresserbrief.ts
+++ 09/erpresserbrief.ts
@@ -8,16 +8,18 @@ Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde n
 
 namespace A09 {
     window.addEventListener("load", init);
-   // window.addEventListener("keydown", handleKeydown);
+    window.addEventListener("keydown", handleKeyDown);
     let text: HTMLElement;
-
+    
+    //String-Array zum Generieren der Buchstaben
     let letters: string[] = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
     let saveLetter: string = "";
-    
+
+    // Div-Boxen für Alphabeth erstellen und gestalten
     
     function init(): void {
-        for (let i: number = 0; i < letters.length; i++) {              //div-Boxen für Alphabeth erstellen und gestalten
+        for (let i: number = 0; i < letters.length; i++) {           
             let l: HTMLDivElement = document.createElement("div");
             l.style.width = "35px";
             l.style.height = "35px";
@@ -26,44 +28,60 @@ namespace A09 {
             l.style.color = "white";
             l.style.fontSize = "25px";
             l.innerText = letters[i];
-            l.id = letters[i]; 
-            l.className = "letters"; 
-
+            l.id = letters[i];
+            l.className = "letters";
 
             l.addEventListener("click", handleLetterClick);
             document.body.appendChild(l);
-     }       
+        }
+
+        //Brieffeld erstellen und gestalten
             
-        let box: HTMLDivElement = document.createElement("div");        //große Textbox erstellen und gestalten
+        let box: HTMLDivElement = document.createElement("div");        
         box.style.width = "1165px";
         box.style.height = "500px";
         box.style.marginTop = "20px";
         box.style.backgroundColor = "#b7b7b7";
+        
         box.addEventListener("click", handleBoxClick);
         document.body.appendChild(box);
     }
-        
-        
 
-        function handleLetterClick(_event: MouseEvent): void {
-        console.log (_event.target);
-        
+
+    // Funktion zum Hervorheben des geklickten Buchstaben
+    
+    function handleLetterClick(_event: MouseEvent): void {      
+        console.log(_event.target);
+
         let l: HTMLDivElement = <HTMLDivElement>_event.target;
         l.style.color = "#ffffff";
         l.style.backgroundColor = "#000000";
         saveLetter = l.id;
-            
+
         let letterList: NodeListOf<HTMLDivElement> = <NodeListOf<HTMLDivElement>>document.getElementsByClassName("letters"); //Jedes Div der Klasse letters in Variable speichern
 
-        for (let i: number = 0; i < letterList.length; i++) { 
+        for (let i: number = 0; i < letterList.length; i++) {   // Wenn der gespeicherte Buchstabe nicht mit der ID aus der Buchstabenliste übereinstimmt, wird die Farbe des Hintergrunds zurückgesetzt 
             if (saveLetter != letterList[i].id) {
                 letterList[i].style.backgroundColor = "#b7b7b7";
             }
         }
-       
-    } 
-        
-        function handleBoxClick(_event: MouseEvent): void {
+    }
+    
+    //Funktion für Tastatureingabe
+    
+    function handleKeyDown(_event: KeyboardEvent): void {
+
+        if (_event.keyCode > 64 && _event.keyCode < 91 || _event.keyCode == 16 || _event.keyCode == 20) {
+            saveLetter = String.fromCharCode(_event.keyCode);
+
+        } else {
+            return;
+        }
+    }
+    
+    // Funktion zum Plazieren des gespeicherten Buchstabens in dem Brieffeld
+    
+    function handleBoxClick(_event: MouseEvent): void {
         if (saveLetter == "")
             return;
 
@@ -74,13 +92,23 @@ namespace A09 {
         text.style.width = "35px";
         text.style.height = "35px";
         text.style.fontSize = "30px";
-        text.style.backgroundColor = "hsl(" + Math.random() * 360 + ", 100%, 80%)";    
+        text.style.backgroundColor = "hsl(" + Math.random() * 360 + ", 100%, 80%)";
         text.style.left = _event.pageX + "px";
         text.style.top = _event.pageY + "px";
-        //text.addEventListener("click", deleteLetter);
+        text.addEventListener("click", deleteLetter);
         document.body.appendChild(text);
+    }
+
+    //Funktion zum Löschen eines Buchstabens durch gedrückte Alt-Taste und Klicken
+
+    function deleteLetter(_event: MouseEvent): void {
+        if (_event.altKey == false)
+            return;
+
+        else {
+            let remove: HTMLDivElement = <HTMLDivElement>_event.target;
+            document.body.removeChild(remove);
         }
-  
-  
-    
- }
+    }
+
+}
